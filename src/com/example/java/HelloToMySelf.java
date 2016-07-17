15fdb5b475271ab44a55c48b295dd01cb886ee50
Mohamed Gassama <bouyagassama@gmail.com>
2016-07-17 17:35:35 -0400

    my java

---
+++ src/com/example/java/HelloToMySelf.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+/**
+ * Created by mohamedbgassama on 7/15/16.
+ */
+public class HelloToMySelf {
+    public static void main(String[] args){
+    String pt = " CEO of Backpack ";
+        boolean te =  pt.isEmpty();
+        System.out.println(te);
+    System.out.println("Mohamed is the" + pt);
+
+
+ }
+}
---
+++ src/com/example/java/Main.java
@@ -0,0 +1,25 @@
+package com.example.java;
+
+/**
+ * Created by mohamedbgassama on 7/13/16.
+ */
+public class Main {
+    public static void  main(String[] args){
+       String s1 = "This is a string";
+        System.out.println(s1);
+
+        String s2 = new String("This is another strings");
+        System.out.println(s2);
+
+        String s3 = "Short size";
+        String s4 = " M";
+        String s5 = s3 + s4 + " Qty: " + 5;
+        System.out.println(s5);
+
+        char[] cahr = {'h','e','l','l','o'};
+        String my = new String(cahr);
+        System.out.println(my);
+
+    }
+
+}
